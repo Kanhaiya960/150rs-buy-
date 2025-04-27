211ce0101687c7155a1b4a4e0c07a61109161d44
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-04-27 10:28:44 +0530

    Update info.py

--- info.py
+++ info.py
@@ -51,8 +51,8 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB info
-SECONDDB_URI = environ.get('SECONDDB_URI', "")
-DATABASE_URI = environ.get('DATABASE_URI', "")
+SECONDDB_URI = environ.get('SECONDDB_URI', "mongodb+srv://sujay53722773:sujay53722773@cluster0298357.mikqelj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0298357")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sujay5372384874:sujay5372384874@cluster0727373.a1qsd0p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0727373")
 DATABASE_NAME = environ.get('DATABASE_NAME', "ALEXA99")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'My_Tg_files')
 
